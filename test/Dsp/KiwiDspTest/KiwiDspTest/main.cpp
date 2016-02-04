51c99034bbcdded0ce3ffdc514185373586d972d
pierreguillot <guillotpierre6@gmail.com>
2016-02-04 06:48:22 +0100

    update dsp samples

--- test/Dsp/KiwiDspTest/KiwiDspTest/main.cpp
+++ test/Dsp/KiwiDspTest/KiwiDspTest/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "KiwiDspSamples.hpp"
+
+using namespace kiwi::dsp;
+
+int main(int, const char*[]) {
+    
+    //int* isamples = Sample<int>::allocate(128);
+    float* fsamples = Samples<float>::allocate(128);
+    double* dsamples = Samples<double>::allocate(128);
+    
+    for(int i = 1; i < 128; i++)
+    {
+        if((reinterpret_cast<char *>(fsamples+i)-sizeof(float)) != (reinterpret_cast<char *>(fsamples+i-1)))
+        {
+            std::cout << "Wrong align float";
+        }
+        if((reinterpret_cast<char *>(dsamples+i)-sizeof(double)) != (reinterpret_cast<char *>(dsamples+i-1)))
+        {
+            std::cout << "Wrong align double";
+        }
+           
+    }
+    std::cout << "Size of vector : " << sizeof(fsamples) << " and should be " << (128 * sizeof(float)) << "\n";
+    std::cout << "Size of vector : " << sizeof(dsamples) << " and should be " << (128 * sizeof(double)) << "\n";
+    Samples<float>::release(fsamples);
+    Samples<double>::release(dsamples);
     return 0;
 }
