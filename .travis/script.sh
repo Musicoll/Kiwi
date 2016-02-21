33aaa723462d739284c3f7a01952a8c92f187089
pierreguillot <guillotpierre6@gmail.com>
2016-02-21 15:47:40 +0100

    [DSP] e

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-COVERALL_SUPPORT = "OFF"
+COVERALL_SUPPORT="OFF"
 if [ $TRAVIS_OS_NAME == 'linux' ] && [ $COMPILER == 'gcc' ]; then
-  $COVERALL_SUPPORT = "-DCOVERALL=ON"
+  $COVERALL_SUPPORT="ON"
 fi
 
 
 
 TEST_RUN = "test_core test_dsp"
 if [ $TRAVIS_BRANCH == 'dev-dsp' ]; then
-  $TEST_RUN = "test_dsp"
+  $TEST_RUN="test_dsp"
 elif [ $TRAVIS_BRANCH == 'dev-core' ]; then
-  $TEST_RUN = "test_core"
+  $TEST_RUN="test_core"
 fi
 
-echo "COVERALL_SUPPORT=" $COVERALL_SUPPORT
-echo "TEST_RUN="$TEST_RUN
+echo "COVERALL_SUPPORT = " $COVERALL_SUPPORT
+echo "TEST_RUN = "$TEST_RUN
 
 export CXX=$COMPILER
 make clean
