d26ae042c240b7f2b5018bf244e69096d55b9bef
pierreguillot <guillotpierre6@gmail.com>
2016-02-21 15:13:25 +0100

    a

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 COVERALL_SUPPORT = -DCOVERALL=OFF
-if [[ $TRAVIS_OS_NAME == 'linux' ] && [ $COMPILER == 'gcc' ]]; then
+if [ $TRAVIS_OS_NAME == 'linux' ] && [ $COMPILER == 'gcc' ]; then
   $COVERALL_SUPPORT = -DCOVERALL=ON
 fi
 
 TEST_SUPPORT
-if [[ $TRAVIS_BRANCH == 'dev-dsp' ]]; then
+if [ $TRAVIS_BRANCH == 'dev-dsp' ]; then
   $TEST_SUPPORT = --target  --target test_dsp
-elif [[ $TRAVIS_BRANCH == 'dev-core' ]]; then
+elif [ $TRAVIS_BRANCH == 'dev-core' ]; then
   $TEST_SUPPORT = --target  --target test_core
 fi
 
