6e3f45b3cd2600de816e47af90b863524ffa23af
pierreguillot <guillotpierre6@gmail.com>
2016-02-21 11:39:34 +0100

    [DSP] travis test

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,5 +3,5 @@
 if [[ $TRAVIS_BRANCH == 'dev-dsp' ]]; then
   $TRAVIS_SUBMODULES = false
 elif [[ $TRAVIS_BRANCH == 'dev-core' ]]; then
-  TRAVIS_SUBMODULES = false
+  $TRAVIS_SUBMODULES = false
 fi
