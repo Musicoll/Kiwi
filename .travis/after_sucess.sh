6f326f75468ac5a45ff87ea308668b755453c944
pierreguillot <guillotpierre6@gmail.com>
2016-02-21 12:13:23 +0100

    [DSP] test travis

--- .travis/after_sucess.sh
+++ .travis/after_sucess.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'dev-dsp' ]]; then
-  ./test_dsp
-elif [[ $TRAVIS_BRANCH == 'dev-core' ]]; then
-  ./test_core
+if [[ $TRAVIS_OS_NAME == 'linux' ] && [ $COMPILER == "g++4.9"]]; then
+  if [[ $TRAVIS_BRANCH == 'dev-dsp' ]]; then
+    ./test_dsp
+  elif [[ $TRAVIS_BRANCH == 'dev-core' ]]; then
+    ./test_core
+  else
+    ./test_core
+    ./test_dsp/
+  fi
 else
-  ./test_core
-  ./test_dsp/
+  if [[ $TRAVIS_BRANCH == 'dev-dsp' ]]; then
+    ./test_dsp
+  elif [[ $TRAVIS_BRANCH == 'dev-core' ]]; then
+    ./test_core
+  else
+    ./test_core
+    ./test_dsp/
+  fi
 fi
