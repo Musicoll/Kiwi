94aa486bdef45b65732fd9ae14b04ed0207c135f
pierreguillot <guillotpierre6@gmail.com>
2016-02-21 15:00:23 +0100

    [DSP] travis

--- .travis/after_sucess.sh
+++ .travis/after_sucess.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-if [[ $TRAVIS_OS_NAME == 'linux' ] && [ $COMPILER == "g++4.9"]]; then
-  if [[ $TRAVIS_BRANCH == 'dev-dsp' ]]; then
-    ./test_dsp
-  elif [[ $TRAVIS_BRANCH == 'dev-core' ]]; then
-    ./test_core
-  else
-    ./test_core
-    ./test_dsp/
-  fi
+if [[ $TRAVIS_OS_NAME == 'linux' ] && [ $COMPILER == 'gcc' ]]; then
+coveralls -E '/build/CMakeFiles/*.cpp' -E '/build/CMakeFiles/*.cxx' -e ThirdParty -e Client -e test --gcov-options '\-lp' --gcov 'gcov-4.9'
 else
   if [[ $TRAVIS_BRANCH == 'dev-dsp' ]]; then
     ./test_dsp
--- .travis/script.sh
+++ .travis/script.sh
@@ -2,14 +2,29 @@
 
 if [[ $TRAVIS_BRANCH == 'dev-dsp' ]]; then
   export CXX=$COMPILER
+  make clean
+  if [[ $TRAVIS_OS_NAME == 'linux' ] && [ $COMPILER == 'gcc' ]]; then
   cmake ./
+  else
+  cmake -DCOVERALL=ON ./
+  fi
   cmake --build ./  --target test_dsp
 elif [[ $TRAVIS_BRANCH == 'dev-core' ]]; then
   export CXX=$COMPILER
+  make clean
+  if [[ $TRAVIS_OS_NAME == 'linux' ] && [ $COMPILER == 'gcc' ]]; then
   cmake ./
+  else
+  cmake -DCOVERALL=ON ./
+  fi
   cmake --build ./  --target test_core
 else
   export CXX=$COMPILER
+  make clean
+  if [[ $TRAVIS_OS_NAME == 'linux' ] && [ $COMPILER == 'gcc' ]]; then
   cmake ./
+  else
+  cmake -DCOVERALL=ON ./
+  fi
   cmake --build ./
 fi
