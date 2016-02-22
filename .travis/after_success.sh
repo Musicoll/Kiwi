d4410445d2d11b501f15280f942f42776b0f4cd6
pierreguillot <guillotpierre6@gmail.com>
2016-02-22 12:29:14 +0100

    [DSP] Correct coverall options

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -6,15 +6,15 @@ if [ $TRAVIS_OS_NAME == 'linux' ] && [ $COMPILER == 'g++-4.9' ]; then
 fi
 
 if [ $COVERALL_SUPPORT == "ON" ]; then
-  
+
   if [ $TRAVIS_BRANCH == 'dev-dsp' ]; then
-    coveralls -E 'CMakeFiles/*.cpp' -E 'CMakeFiles/*.cxx' -e ThirdParty -e Client -e test -e Modules/KiwiCore -e Modules/KiwiGraphics --gcov-options 'gcov-4.9' --gcov '\-lp'
+    coveralls -E 'CMakeFiles/*.cpp' -E 'CMakeFiles/*.cxx' -e ThirdParty -e Client -e test -e Modules/KiwiCore -e Modules/KiwiGraphics --gcov-options '\-lp' --gcov 'gcov-4.9'
   elif [ $TRAVIS_BRANCH == 'dev-core' ]; then
-    coveralls -E 'CMakeFiles/*.cpp' -E 'CMakeFiles/*.cxx' -e ThirdParty -e Client -e test  -e Modules/KiwiDsp -e Modules/KiwiGraphics --gcov-options 'gcov-4.9' --gcov '\-lp'
+    coveralls -E 'CMakeFiles/*.cpp' -E 'CMakeFiles/*.cxx' -e ThirdParty -e Client -e test  -e Modules/KiwiDsp -e Modules/KiwiGraphics --gcov-options '\-lp' --gcov 'gcov-4.9'
   elif [ $TRAVIS_BRANCH == 'dev-graphics' ]; then
-    coveralls -E 'CMakeFiles/*.cpp' -E 'CMakeFiles/*.cxx' -e ThirdParty -e Client -e test  -e Modules/KiwiDsp -e Modules/KiwiGraphics --gcov-options 'gcov-4.9' --gcov '\-lp'
+    coveralls -E 'CMakeFiles/*.cpp' -E 'CMakeFiles/*.cxx' -e ThirdParty -e Client -e test  -e Modules/KiwiDsp -e Modules/KiwiGraphics --gcov-options '\-lp' --gcov 'gcov-4.9'
   else
-    coveralls -E 'CMakeFiles/*.cpp' -E 'CMakeFiles/*.cxx' -e ThirdParty -e Client -e test  --gcov-options 'gcov-4.9' --gcov '\-lp'
+    coveralls -E 'CMakeFiles/*.cpp' -E 'CMakeFiles/*.cxx' -e ThirdParty -e Client -e test  --gcov-options '\-lp' --gcov 'gcov-4.9'
   fi
 
 fi
