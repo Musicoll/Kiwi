ad6301f9b5ab0634e1cbd9b7fa1d1b7b0b395cc2
jean.millot <jean@jmillot-musicoll.local>
2016-03-10 15:08:58 +0100

    [Implementation Path, Point] Changing commentary layout

--- Modules/KiwiGraphics/KiwiPath.hpp
+++ Modules/KiwiGraphics/KiwiPath.hpp
@@ -36,106 +36,106 @@ namespace kiwi
         //                                      PATH                                        //
         // ================================================================================ //
         
-        //!@brief The path holds a set of points.
-        //!@details The path is used to represent a set of curves and shapes. It can be discontinuous.
-        //!@todo Check if constructors with shapes are needed
-        //!@todo Check if withdrawn functions are needed (addArc, addPieChart, addArc, overlaps)
-        //!@todo Check if bounds are needed and how they can be implemented (dizcretisation, recompute at every change)
+        //! @brief The path holds a set of points.
+        //! @details The path is used to represent a set of curves and shapes. It can be discontinuous.
+        //! @todo Check if constructors with shapes are needed
+        //! @todo Check if withdrawn functions are needed (addArc, addPieChart, addArc, overlaps)
+        //! @todo Check if bounds are needed and how they can be implemented (dizcretisation, recompute at every change)
         class Path
         {
         public:
-            //!@brief Default constructor
-            //!@details Initializes a path positioned at the origin
+            //! @brief Default constructor
+            //! @details Initializes a path positioned at the origin
             Path() noexcept {addNode(Node(Point(), Move));};
             
-            //!@brief Copy constructor
-            //!@details Copies all the nodes of a path
+            //! @brief Copy constructor
+            //! @details Copies all the nodes of a path
             Path(Path const& path) noexcept : m_nodes(path.m_nodes) {};
             
-            //!@brief Move constructor
-            //!@details Moves the nodes to the new constructed path
+            //! @brief Move constructor
+            //! @details Moves the nodes to the new constructed path
             Path(Path&& other) noexcept : m_nodes(std::move(other.m_nodes)){};
             
-            //!@brief Position constructor
-            //!@details Initializes a constructor at a certain position
+            //! @brief Position constructor
+            //! @details Initializes a constructor at a certain position
             explicit Path(Point const& position) noexcept {addNode(Node(position, Move));}
             
-            //!@brief Assignment operator
-            //!@details Clears all nodes and copies all nodes from other
+            //! @brief Assignment operator
+            //! @details Clears all nodes and copies all nodes from other
             Path& operator=(Path const& other) noexcept;
             
-            //!@brief Move assignment operator
-            //!@details Moves the nodes from other
+            //! @brief Move assignment operator
+            //! @details Moves the nodes from other
             Path& operator=(Path&& other) noexcept;
             
-            //@brief Destructor
+            //! @brief Destructor
             ~Path() noexcept = default;
             
-            //!@brief Gets the number of nodes
+            //! @brief Gets the number of nodes
             inline ulong size() const noexcept {return static_cast<ulong>(m_nodes.size());}
             
-            //!@brief Returns true if the path is empty
+            //! @brief Returns true if the path is empty
             inline bool empty() const noexcept {return m_nodes.size() <= 1;}
             
-            //!@brief Removes all nodes
-            //!@details After clear, the path continues drawing from origin
+            //! @brief Removes all nodes
+            //! @details After clear, the path continues drawing from origin
             void clear() noexcept;
             
-            //!@brief Applies an affine transform to the path
-            //!@details AffineTransform is applied to all nodes
+            //! @brief Applies an affine transform to the path
+            //! @details AffineTransform is applied to all nodes
             void transform(AffineMatrix const& matrix) noexcept;
             
-            //!@brief Gets a affine transformed path
-            //!@details Retrieves a copy, doesn't apply changes to path
+            //! @brief Gets a affine transformed path
+            //! @details Retrieves a copy, doesn't apply changes to path
             Path transformed(AffineMatrix const& matrix) const noexcept;
             
-            //!@brief Moves drawing cursor to the position
-            //!@details Doesn't actually move the nodes, sets the next drawing position instead. Paths can be discontinuous.
+            //! @brief Moves drawing cursor to the position
+            //! @details Doesn't actually move the nodes, sets the next drawing position instead. Paths can be discontinuous.
             void moveTo(Point const& position) noexcept;
 
-            //!@brief Adds a line to the current path
-            //!@param line_edge Draws a line from cursor to line_edge
+            //! @brief Adds a line to the current path
+            //! @param line_edge Draws a line from cursor to line_edge
             void addLine(Point const& line_edge) noexcept;
             
-            //!@brief Adds a sets of line
-            //!@param il Contains the edges position of the added lines
+            //! @brief Adds a sets of line
+            //! @param il Contains the edges position of the added lines
             void addLines(std::initializer_list<Point> il) noexcept;
             
-            //!@brief Adds a quadradic bezier curve
+            //! @brief Adds a quadradic bezier curve
             void addQuadratic(Point const& control, Point const& end) noexcept;
             
-            //!@brief Adds a set of quadratic bezier curves
-            //!@param il contains the control positions for every quadradic curve
+            //! @brief Adds a set of quadratic bezier curves
+            //! @param il contains the control positions for every quadradic curve
             void addQuadratics(std::initializer_list<std::array<Point, 2>> il) noexcept;
             
-            //!@brief Adds a cubic curve to the path
+            //! @brief Adds a cubic curve to the path
             void addCubic(Point const& control1, Point const& control2, Point const& end) noexcept;
             
-            //!@brief Adds a set of cubic bezier curves
-            //!@param il Contains the control positions for every cubic curve
+            //! @brief Adds a set of cubic bezier curves
+            //! @param il Contains the control positions for every cubic curve
             void addCubics(std::initializer_list<std::array<Point, 3>> il) noexcept;
             
-            //!@brief Adds rectangle to the path
-            //!@param top_left The top_left corner position of the added rectangle
-            //!@param width Horizontal width of the rectangle
-            //!@param height Vertical height of the the rectangle
+            //! @brief Adds rectangle to the path
+            //! @param top_left The top_left corner position of the added rectangle
+            //! @param width Horizontal width of the rectangle
+            //! @param height Vertical height of the the rectangle
             void addRectangle(Point const& top_left, const double width, const double height) noexcept;
             
-            //!@brief Add an ellipse to the path
-            //!@param center Center of the ellipse
-            //!@param radius_x Horizontal radius
-            //!@param radius_y Vertical radius
+            //! @brief Add an ellipse to the path
+            //! @param center Center of the ellipse
+            //! @param radius_x Horizontal radius
+            //! @param radius_y Vertical radius
             void addEllipse(Point const& center, const double radius_x, const double radius_y) noexcept;
             
-            //!@brief Appends a path
+            //! @brief Appends a path
             void addPath(Path const& path) noexcept;
             
-            //!@brief Closes the final edge of the path
-            //!@details If the path is discontinuous, only the final part of the path is closed
+            //! @brief Closes the final edge of the path
+            //! @details If the path is discontinuous, only the final part of the path is closed
             void close() noexcept;
             
         private:
-            //!@internal
+            //! @internal
             enum Mode
             {
                 Close       = 0,
@@ -145,7 +145,7 @@ namespace kiwi
                 Cubic       = 4
             };
             
-            //!@internal
+            //! @internal
             class Node
             {
             public:
@@ -170,15 +170,15 @@ namespace kiwi
                 Mode  m_mode;
             };
             
-            //!@internal
+            //! @internal
             bool haveSamePos(Node const& node_l, Node const& node_r) const noexcept;
             
             std::vector<Node> m_nodes;
             
-            //@!internal
+            //! @internal
             Node lastMoveNode() const noexcept;
             
-            //!@internal
+            //! @internal
             void addNode(Node const& node) noexcept{m_nodes.push_back(node);}
         };
     }
--- Modules/KiwiGraphics/KiwiPoint.hpp
+++ Modules/KiwiGraphics/KiwiPoint.hpp
@@ -37,52 +37,47 @@ namespace kiwi
         {
         public:
             
-            //!@brief Default Constructor
-            //!@details Initializes with null coordinates
-            constexpr inline Point() noexcept : m_data{0., 0.}{}
+            //! @brief Default Constructor
+            //! @details Initializes with null coordinates
+            constexpr Point() noexcept : m_data{0., 0.}{}
             
-            //! Constructor.
-            /** The function initializes a point with two double values.
-             @param x The abscissa.
-             @param y The ordinate.
-             */
-            //!@brief Constructor
-            //!@details Initializes the coordinates
-            constexpr inline Point(const double x, const double y) noexcept : m_data{x, y}{}
+            //! @brief Constructor
+            //! @details Initializes the coordinates
+            explicit constexpr Point(const double x, const double y) noexcept : m_data{x, y}{}
             
-            //!@brief Copy Constrctor
-            //!@details Initializes with same coordinates
-            constexpr inline Point(Point const& pt) noexcept : m_data{pt.m_data[0], pt.m_data[1]}{}
+            //! @brief Copy Constrctor
+            //! @details Initializes with same coordinates
+            constexpr Point(Point const& pt) noexcept : m_data{pt.m_data[0], pt.m_data[1]}{}
             
-            //!@brief Desctructor
+            //! @brief Desctructor
             inline ~Point() noexcept = default;
             
-            //!@brief Gets the abscissa
+            //! @brief Gets the abscissa
             constexpr inline double x() const noexcept
             {
                 return m_data[0];
             }
             
-            //@brief Gets the ordinate
+            // @brief Gets the ordinate
             constexpr inline double y() const noexcept
             {
                 return m_data[1];
             }
             
-            //!@brief Sets the abscissa
+            //! @brief Sets the abscissa
             inline void x(const double x) noexcept
             {
                 m_data[0] = x;
             }
             
-            //!@brief Sets the ordinate
+            //! @brief Sets the ordinate
             inline void y(const double y) noexcept
             {
                 m_data[1] = y;
             }
             
-            //!@brief Copies the coordinate of another point
-            inline Point& operator=(Point const& pt)
+            //! @brief Copies the coordinate of another point
+            Point& operator=(Point const& pt) noexcept
             {
                 if (&pt != this)
                 {
@@ -92,88 +87,83 @@ namespace kiwi
                 return *this;
             }
             
-            //!@brief Retrieves a copy of the rotated point around the origin
-            //!@return A copy of the rotated Point
+            //! @brief Retrieves a copy of the rotated point around the origin
+            //! @return A copy of the rotated Point
             Point rotated(double const angle) const noexcept;
             
-            //!@brief Retrieves a copy of the rotated point around another pt
-            //!@param pt The rotation center
-            //!@return A copy of the rotated point around another pt
+            //! @brief Retrieves a copy of the rotated point around another pt
+            //! @param pt The rotation center
+            //! @return A copy of the rotated point around another pt
             Point rotated(Point const& pt, double const angle) const noexcept;
             
-            //! Apply a rotation from the origin.
-            /** The function retrieves Applies a rotation from the origin
-             @param angle The angle in radian
-             */
-            
-            //!@brief Applies a rotation around the origin
+            //! @brief Applies a rotation around the origin
             void rotate(double const angle) noexcept;
             
-            //!@brief Applies a rotation arount pt
-            //!@param pt The rotation center
+            //! @brief Applies a rotation arount pt
+            //! @param pt The rotation center
             void rotate(Point const& pt, double const angle) noexcept;
             
-            //! Retrieves the angle in radian from abscissa clockwise
-            //!@return Angle in radian
+            //! @brief Retrieves the angle in radian from abscissa clockwise
+            //! @return Angle in radian
             double angle() const noexcept;
             
-            //!@brief Retrieves the angle to another point
-            //!@details Angle of the vector between the two points
+            //! @brief Retrieves the angle to another point
+            //! @details Angle of the vector between the two points
             double angleToPoint(Point const& pt) const noexcept;
             
-            //!@brief Returns the scalar product of two points
+            //! @brief Returns the scalar product of two points
             double dot(Point const& pt) const noexcept;
             
         private:
             double m_data[2];
         };
         
-        //!@brief Retrun true if the distance between the points is less or equal to eps
+        //! @brief Retrun true if the distance between the points is less or equal to eps
         bool areNear(Point const& l_pt, Point const& r_pt, const double eps) noexcept;
         
-        //!@brief Return true if the distance between points is less or equal to an epsilon
-        //!@details The default epsilon used is 1e-12
+        //! @brief Return true if the distance between points is less or equal to an epsilon
+        //! @details The default epsilon used is 1e-12
         bool areNear(Point const& l_pt, Point const& r_pt) noexcept;
         
-        //!@brief Retrieves the distance from the origin
+        //! @brief Retrieves the distance from the origin
         double norm(Point const& pt) noexcept;
         
-        //!@brief Returns the distance between two points
+        //! @brief Returns the distance between two points
         double distance(Point const &lPt, Point const& rPt) noexcept;
         
-        //!@brief Multiplies point with scalar value
-        //!@details Returns a point with both coordinates multiplied by value
+        //! @brief Multiplies point with scalar value
+        //! @details Returns a point with both coordinates multiplied by value
         Point operator*(const double value, Point const& pt) noexcept;
         
-        //!@brief Multiplies point with scalar value
-        //!@details Returns a point with both coordinates multiplied by value
+        //! @brief Multiplies point with scalar value
+        //! @details Returns a point with both coordinates multiplied by value
         Point operator*(Point const& pt, double const value) noexcept;
         
-        //!@brief Mutltiplies the coordinates by value
+        //! @brief Mutltiplies the coordinates by value
         Point& operator*=(Point &pt, double const value) noexcept;
         
-        //!@brief Divides point with scalar value
-        //!@details Returns a point with both coordinates divided by value
+        //! @brief Divides point with scalar value
+        //! @details Returns a point with both coordinates divided by value
         Point operator/(Point const& pt, const double value);
         
-        //!@brief Divides the abscissa and the ordinate with a value.
+        //! @brief Divides the abscissa and the ordinate with a value.
         Point& operator/=(Point & pt, double const value);
         
-        //!@brief Retrieves a point with the added ccordinates of lPt and rPt
-        //!@details l_pt + r_pt
+        //! @brief Retrieves a point with the added ccordinates of lPt and rPt
+        //! @details l_pt + r_pt
         Point operator+(Point const& lPt, Point const & rPt) noexcept;
         
-        //!@brief Increments the coordinates with another point's coordinates
+        //! @brief Increments the coordinates with another point's coordinates
         Point& operator+=(Point &lPt, Point const& rPt);
         
-        //!@brief Retrieves a point with the substraction of lPt and rPt coordinates
-        //!@details l_pt - r_pt
+        //! @brief Retrieves a point with the substraction of lPt and rPt coordinates
+        //! @details l_pt - r_pt
         Point operator-(Point const& lPt, Point const& rPt) noexcept;
         
-        //!@brief Decrements the coordinates with another point's coordinates
+        //! @brief Decrements the coordinates with another point's coordinates
         Point& operator-=(Point &lPt, Point const& rPt) noexcept;
         
-        //!@brief Returns a point with -abscissa and -ordinate
+        //! @brief Returns a point with -abscissa and -ordinate
         Point operator-(Point const& pt) noexcept;
     }
 }
