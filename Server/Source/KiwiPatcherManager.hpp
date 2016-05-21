4006bfe74df0e2f834ae8f9909214f8c88115cbb
jean-millot <jean.millot7@gmail.com>
2016-05-21 19:00:30 +0200

    Adding kiwi server

---
+++ Server/Source/KiwiPatcherManager.hpp
@@ -0,0 +1,79 @@
+/*
+ ==============================================================================
+ 
+ This file is part of the KIWI library.
+ Copyright (c) 2014 Pierre Guillot & Eliott Paris.
+ 
+ Permission is granted to use this software under the terms of either:
+ a) the GPL v2 (or any later version)
+ b) the Affero GPL v3
+ 
+ Details of these licenses can be found at: www.gnu.org/licenses
+ 
+ KIWI is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ 
+ ------------------------------------------------------------------------------
+ 
+ To release a closed-source product which uses KIWI, contact : guillotpierre6@gmail.com
+ 
+ ==============================================================================
+ */
+
+#ifndef KIWI_SERVER_PATCHER_MANAGER_HPP_INCLUDED
+#define KIWI_SERVER_PATCHER_MANAGER_HPP_INCLUDED
+
+#include <atomic>
+#include <thread>
+
+#include "KiwiPatcherValidator.hpp"
+
+#include <KiwiModel/KiwiPatcher.hpp>
+
+#include <flip/DocumentServer.h>
+#include <flip/contrib/transport_tcp/PortTransportServerTcp.h>
+#include <flip/detail/PortBase.h>
+#include <flip/DocumentObserver.h>
+
+namespace kiwi
+{
+    namespace server
+    {
+        class PatcherManager
+        {
+        public:
+            PatcherManager(uint64_t document_id);
+            
+            ~PatcherManager();
+            
+        private:
+            void launchTransport();
+            void runTransport();
+            void stopTransport();
+            
+            void populatePatcher();
+            
+            model::Patcher &getPatcher();
+            
+            void on_connecting(flip::PortBase& port);
+            void on_connected(flip::PortBase& port);
+            void on_disconnected(flip::PortBase& port);
+            
+        private:
+            PatcherValidator                m_validator;
+            flip::DocumentServer            m_document;
+            flip::PortTransportServerTcp    m_transport;
+            std::thread                     m_transport_loop;
+            std::atomic_bool                m_transport_running;
+            
+        private:
+            PatcherManager(PatcherManager const& other) = delete;
+            PatcherManager(PatcherManager && other) = delete;
+            PatcherManager &operator=(PatcherManager const& other) = delete;
+            PatcherManager &operator=(PatcherManager && other) = delete;
+        };
+    }
+}
+
+#endif //KIWI_SERVER_PATCHER_MANAGER_HPP_INCLUDED
---
+++ Server/Source/KiwiServer.hpp
@@ -0,0 +1,61 @@
+/*
+ ==============================================================================
+ 
+ This file is part of the KIWI library.
+ Copyright (c) 2014 Pierre Guillot & Eliott Paris.
+ 
+ Permission is granted to use this software under the terms of either:
+ a) the GPL v2 (or any later version)
+ b) the Affero GPL v3
+ 
+ Details of these licenses can be found at: www.gnu.org/licenses
+ 
+ KIWI is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ 
+ ------------------------------------------------------------------------------
+ 
+ To release a closed-source product which uses KIWI, contact : guillotpierre6@gmail.com
+ 
+ ==============================================================================
+ */
+
+#ifndef KIWI_SERVER_HPP_INCLUDED
+#define KIWI_SERVER_HPP_INCLUDED
+
+#include <map>
+
+#include "KiwiPatcherManager.hpp"
+
+namespace kiwi
+{
+    namespace server
+    {
+        class Server
+        {
+        public:
+            Server() = default;
+            
+            void runCommand();
+            void runTransport();
+            
+            ~Server() = default;
+            
+        private:
+            void openPatcher(uint64_t document_id);
+            void closePatcher(uint64_t patcher_id);
+            
+        private:
+            std::map<uint64_t, PatcherManager>        m_patchers;
+            
+        private:
+            Server(Server const& other) = delete;
+            Server(Server &&other) = delete;
+            Server &operator=(Server const& other) = delete;
+            Server &operator=(Server &&other) = delete;
+        };
+    }
+}
+
+#endif // KIWI_SERVER_HPP_INCLUDED
