67cc3497e54e18cb2944bd0ea5ff84540433bc76
pierreguillot <guillotpierre6@gmail.com>
2016-06-10 15:18:05 +0200

    ouaip

--- config/publish-doxygen.sh
+++ config/publish-doxygen.sh
@@ -7,18 +7,15 @@ git clone -b gh-pages git@github.com:Musicoll/Kiwi.git --single-branch html
 
 # rm all the files through git to prevent stale files.
 cd html
-ls
-git rm -rf
-cd ..
+git rm -rf .
+cd -
 
 # Generate the HTML documentation.
 doxygen Doxyfile
-ls
 
 # Create and commit the documentation repo.
 cd html
-ls
 git add .
 git commit -m "Automated documentation build."
 git push origin gh-pages
-cd ..
+cd -
