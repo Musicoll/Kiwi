8249c8f43d5af4dba1253a69ba8180c29e9675a0
pierreguillot <guillotpierre6@gmail.com>
2016-06-10 12:27:46 +0200

    re doxy

---
+++ config/publish-doxygen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+# Get doxygen
+sudo apt-get install --yes doxygen graphviz
+
+# Get a clean version of the HTML documentation repo.
+rm -rf html
+mkdir -p html
+git clone -b gh-pages git@github.com:Musicoll/Kiwi.git --single-branch html
+
+# rm all the files through git to prevent stale files.
+cd html
+git rm -rf .
+cd -
+
+# Generate the HTML documentation.
+make doxygen
+
+# Create and commit the documentation repo.
+cd html
+git add .
+git commit -m "Automated documentation build."
+git push origin gh-pages
+cd -
