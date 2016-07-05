7b07610d69d9de6901c4aa6428be484f2828a58f
Pierre <guillotpierre6@gmail.com>
2016-07-05 17:56:18 +0200

    add beacon to engine #20

---
+++ Modules/KiwiEngine/KiwiEngineBeacon.cpp
@@ -0,0 +1,99 @@
+/*
+ ==============================================================================
+ 
+ This file is part of the KIWI library.
+ - Copyright (c) 2014-2016, Pierre Guillot & Eliott Paris.
+ - Copyright (c) 2016, CICM, ANR MUSICOLL, Eliott Paris, Pierre Guillot, Jean Millot.
+ 
+ Permission is granted to use this software under the terms of the GPL v2
+ (or any later version). Details can be found at: www.gnu.org/licenses
+ 
+ KIWI is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+ ------------------------------------------------------------------------------
+ 
+ Contact : cicm.mshparisnord@gmail.com
+ 
+ ==============================================================================
+ */
+
+
+#include "KiwiEngineBeacon.hpp"
+
+namespace kiwi
+{
+    namespace engine
+    {
+        // ================================================================================ //
+        //                                      BEACON                                      //
+        // ================================================================================ //
+        
+        Beacon::Beacon(std::string const& name) : m_name(name)
+        {
+            ;
+        }
+        
+        Beacon::~Beacon()
+        {
+            
+        }
+        
+        void Beacon::bind(Castaway& castaway)
+        {
+            m_castaways.insert(&castaway);
+        }
+        
+        void Beacon::unbind(Castaway& castaway)
+        {
+            m_castaways.erase(&castaway);
+        }
+        
+        void Beacon::dispatch(std::vector<Atom> const& args)
+        {
+            for(auto castaway : m_castaways)
+            {
+                castaway->receive(args);
+            }
+        }
+        
+        // ================================================================================ //
+        //                                  BEACON FACTORY                                  //
+        // ================================================================================ //
+        
+        Beacon::Factory::Factory()
+        {
+            ;
+        }
+        
+        Beacon::Factory::~Factory()
+        {
+            ;
+        }
+        
+        Beacon& Beacon::Factory::get(std::string const& name)
+        {
+            auto it = m_beacons.find(name);
+            if(it != m_beacons.end())
+            {
+                return *it->second.get();
+            }
+            Beacon* beacon = new Beacon(name);
+            if(beacon)
+            {
+                m_beacons[name] = std::unique_ptr<Beacon>(beacon);
+            }
+            return *beacon;
+        }
+    };
+};
+
+
+
+
+
+
+
+
+
---
+++ Modules/KiwiEngine/KiwiEngineBeacon.hpp
@@ -0,0 +1,98 @@
+/*
+ ==============================================================================
+ 
+ This file is part of the KIWI library.
+ - Copyright (c) 2014-2016, Pierre Guillot & Eliott Paris.
+ - Copyright (c) 2016, CICM, ANR MUSICOLL, Eliott Paris, Pierre Guillot, Jean Millot.
+ 
+ Permission is granted to use this software under the terms of the GPL v2
+ (or any later version). Details can be found at: www.gnu.org/licenses
+ 
+ KIWI is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+ ------------------------------------------------------------------------------
+ 
+ Contact : cicm.mshparisnord@gmail.com
+ 
+ ==============================================================================
+ */
+
+#ifndef KIWI_ENGINE_BEACON_HPP_INCLUDED
+#define KIWI_ENGINE_BEACON_HPP_INCLUDED
+
+#include "KiwiEngineDef.hpp"
+
+namespace kiwi
+{
+    namespace engine
+    {
+        // ================================================================================ //
+        //                                      BEACON                                      //
+        // ================================================================================ //
+        
+        //! The beacon is unique and matchs to a "unique" string in the scope of a beacon factory and can be used to bind beacon's castaways.
+        /**
+         The beacon are uniques in the scope of a beacon factory and matchs to a string. If you create a beacon with a string that already matchs to a beacon of the beacon factory, it will return this beacon otherwise it will create a new beacon. Thus, the beacons can be used to bind, unbind and retrieve castways. After recovering a castaway, you should cast dynamicaly it the class you expect. More often, this will be an kiwi object.
+         @see Beacon::Factory
+         @see Beacon::Castaway
+         */
+        class Beacon
+        {
+        public:
+            class Castaway;
+            
+            //! @brief Gets the name of the beacon.
+            inline std::string getName() const {return m_name;}
+            
+            ~Beacon();
+            
+            //! @brief Adds a castaway in the binding list of the beacon.
+            void bind(Castaway& castaway);
+            
+            //! @brief Removes a castaways from the binding list of the beacon.
+            void unbind(Castaway& castaway);
+            
+            //! @brief ...
+            void dispatch(std::vector<Atom> const& args);
+            
+            // ================================================================================ //
+            //                                  BEACON CASTAWAY                                 //
+            // ================================================================================ //
+            
+            //! @brief The beacon castaway can be attached to a beacon.
+            class Castaway
+            {
+            public:
+                virtual ~Castaway() {}
+                virtual void receive(std::vector<Atom> const& args) = 0;
+            };
+            
+            // ================================================================================ //
+            //                                  BEACON FACTORY                                  //
+            // ================================================================================ //
+            class Factory
+            {
+            public:
+                Factory();
+                ~Factory();
+                Beacon& get(std::string const& name);
+            private:
+                std::map<std::string, std::unique_ptr<Beacon>> m_beacons;
+            };
+            
+        private:
+            Beacon(std::string const& name);
+            friend class Factory;
+        private:
+            const std::string            m_name;
+            std::set<Castaway *>    m_castaways;
+        };
+    };
+};
+
+
+#endif // KIWI_ENGINE_BEACON_HPP_INCLUDED
+
+
