d65c4d37f6f2130722613bfd5eab817d392a1f03
jean-millot <jean.millot7@gmail.com>
2016-10-07 13:04:03 +0200

    Enabling configuring in mode flip private only for mac.

--- Config/configure.py
+++ Config/configure.py
@@ -13,15 +13,15 @@ project_dir = os.path.abspath(".");
 
 cmd = "cmake";
 
-private_attr = ""
-
 #==============================================================================
 # Name : parse_args
 #==============================================================================
 
 def parse_args ():
     arg_parser = argparse.ArgumentParser ()
-    arg_parser.add_argument('-flip', default = 'public', choices = ['public', 'private'])
+    
+    if platform.system () == 'Darwin':
+        arg_parser.add_argument('-flip', default = 'public', choices = ['public', 'private'])
 
     return arg_parser.parse_args (sys.argv[1:])
 
@@ -29,17 +29,14 @@ def parse_args ():
 # Name : flip_private
 #==============================================================================
 
-def flip_private(args):
-    global private_attr;
-    if(args.flip == "private"):
-        flip_path = os.path.join(project_dir, "ThirdParty", "flip")
-        os.chdir(flip_path);
-        configure_path = os.path.join(flip_path, "configure.py")
-        build_path = os.path.join(flip_path, "build.py")
-        subprocess.check_call("python " + configure_path, shell= True);
-        subprocess.check_call("python " + build_path + " -c Debug -t flip", shell= True);
-        subprocess.check_call("python " + build_path + " -c Release -t flip", shell= True);
-        private_attr += " -DFLIP_PRIVATE=1";
+def build_flip_private():
+    flip_path = os.path.join(project_dir, "ThirdParty", "flip")
+    os.chdir(flip_path);
+    configure_path = os.path.join(flip_path, "configure.py")
+    build_path = os.path.join(flip_path, "build.py")
+    subprocess.check_call("python " + configure_path, shell= True);
+    subprocess.check_call("python " + build_path + " -c Debug -t flip", shell= True);
+    subprocess.check_call("python " + build_path + " -c Release -t flip", shell= True);
 
 
 #==============================================================================
@@ -61,7 +58,6 @@ def mac_release():
     build_dir = os.path.join(project_dir, "Build", "Release");
     os.makedirs(build_dir);
     os.chdir(build_dir);
-    cmd_loc+= private_attr;
     cmd_loc+= " -DCMAKE_BUILD_TYPE:STRING=Release";
     cmd_loc+= " " + project_dir;
     subprocess.check_call(cmd_loc, shell= True);
@@ -77,7 +73,6 @@ def mac_debug():
     build_dir = os.path.join(project_dir, "Build", "Debug");
     os.makedirs(build_dir);
     os.chdir(build_dir);
-    cmd_loc+= private_attr;
     cmd_loc += " -DCMAKE_BUILD_TYPE:STRING=Debug"
     cmd_loc += " " + project_dir;
     subprocess.check_call(cmd_loc, shell= True);
@@ -87,12 +82,15 @@ def mac_debug():
 # Name : configure_mac
 #==============================================================================
 
-def configure_mac():
-	global cmd;
-	cmd += " -G Xcode";
-	mac_debug();
-	mac_release();
-	return
+def configure_mac(args):
+    global cmd;
+    cmd += " -G Xcode";
+    if (args.flip == "private"):
+        build_flip_private()
+        cmd += " -DFLIP_PRIVATE=1"
+    mac_debug();
+    mac_release();
+    return
 
 #==============================================================================
 # Name : win_debug_32
@@ -104,7 +102,6 @@ def win_debug_32():
     os.makedirs(build_dir);
     os.chdir(build_dir);
     cmd_loc += "\""
-    cmd_loc+= private_attr;
     cmd_loc += " -DCMAKE_BUILD_TYPE:STRING=Debug"
     cmd_loc += " " + project_dir
     subprocess.check_call(cmd_loc, shell= True)
@@ -120,7 +117,6 @@ def win_debug_64():
     os.makedirs(build_dir);
     os.chdir(build_dir);
     cmd_loc += " Win64\""
-    cmd_loc+= private_attr;
     cmd_loc += " -DCMAKE_BUILD_TYPE:STRING=Debug"
     cmd_loc += " " + project_dir
     subprocess.check_call(cmd_loc, shell= True)
@@ -135,7 +131,6 @@ def win_release_32():
     build_dir = os.path.join(project_dir, "Build", "Release", "Win32");
     os.makedirs(build_dir);
     os.chdir(build_dir);
-    cmd_loc+= private_attr;
     cmd_loc += "\""
     cmd_loc += " -DCMAKE_BUILD_TYPE:STRING=Release"
     cmd_loc += " " + project_dir
@@ -152,7 +147,6 @@ def win_release_64():
     build_dir = os.path.join(project_dir, "Build", "Release", "x64");
     os.makedirs(build_dir);
     os.chdir(build_dir);
-    cmd_loc+= private_attr;
     cmd_loc += " Win64\""
     cmd_loc += " -DCMAKE_BUILD_TYPE:STRING=Release"
     cmd_loc += " " + project_dir
@@ -163,7 +157,7 @@ def win_release_64():
 # Name : configure_windows
 #==============================================================================
 
-def configure_windows():
+def configure_windows(args):
     global cmd
     cmd += " -G \"Visual Studio 14 2015"
     win_debug_32();
@@ -185,9 +179,7 @@ os.makedirs(root_build_dir);
 
 create_dir(os.path.join(project_dir, "Build"))
 
-flip_private(parse_args())
-
 if platform.system()=="Darwin":
-    configure_mac();
+    configure_mac(parse_args());
 elif platform.system() == "Windows":
-	configure_windows()
+	configure_windows(parse_args())
