da175a810a2de5c4ebb5ef2d4aa8639d0470d176
eliottparis <eliottparis@gmail.com>
2016-10-21 03:14:17 +0200

    fix bad index in receive method (?)

--- Modules/KiwiEngine/KiwiEngine_Link.cpp
+++ Modules/KiwiEngine/KiwiEngine_Link.cpp
@@ -51,12 +51,14 @@ namespace kiwi
         
         size_t Link::getSenderIndex() const
         {
-            return m_model.getSenderIndex(model::PinType::IType::Control);
+            //return m_model.getSenderIndex(model::PinType::IType::Control);
+            return m_model.getSenderIndex();
         }
         
         size_t Link::getReceiverIndex() const
         {
-            return m_model.getReceiverIndex(model::PinType::IType::Control);
+            //return m_model.getReceiverIndex(model::PinType::IType::Control);
+            return m_model.getReceiverIndex();
         }
     }
 }
