3e8ecfcb91799431d63a006ea3fb263157699685
eliott PARIS <eliottparis@gmail.com>
2017-03-31 14:12:08 +0200

    fixup! Renamed closeAllWindows method into closeAllPatcherWindows

--- Client/Source/KiwiApp_Instance.cpp
+++ Client/Source/KiwiApp_Instance.cpp
@@ -60,7 +60,7 @@ namespace kiwi
     
     Instance::~Instance()
     {
-        closeAllWindows();
+        closeAllPatcherWindows();
     }
     
     uint64_t Instance::getUserId() const noexcept
