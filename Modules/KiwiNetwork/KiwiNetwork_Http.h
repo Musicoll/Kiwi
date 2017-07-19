4db1d6e7a077a7fb6ed585c9f6040ad6cc043e29
eliott PARIS <eliottparis@gmail.com>
2017-07-19 13:39:42 +0200

    Add a Http class and put definition in a hpp file

--- Modules/KiwiNetwork/KiwiNetwork_Http.h
+++ Modules/KiwiNetwork/KiwiNetwork_Http.h
@@ -33,195 +33,111 @@
 #include <beast/http.hpp>
 #include <beast/core.hpp>
 
-using tcp = boost::asio::ip::tcp;
-
-namespace kiwi
-{
-    namespace network
+namespace kiwi { namespace network {
+    
+    using tcp = boost::asio::ip::tcp;
+    
+    // ================================================================================ //
+    //                                        HTTP                                      //
+    // ================================================================================ //
+    
+    class Http
     {
-        template<class ReqType, class ResType>
-        class HttpQuery
-        {
-        public: // methods
-            
-            HttpQuery(std::unique_ptr<beast::http::request<ReqType>> request,
-                       std::string port,
-                       std::chrono::milliseconds time_out = std::chrono::milliseconds(0)):
-            m_request(std::move(request)),
-            m_response(),
-            m_port(port),
-            m_io_service(),
-            m_socket(m_io_service),
-            m_error(),
-            m_time_out(time_out),
-            m_timer(m_io_service),
-            m_buffer()
-            {
-            }
-            
-            ~HttpQuery()
-            {
-            }
-            
-            beast::http::response<ResType> writeRequest(beast::error_code& error)
-            {
-                if (m_time_out > std::chrono::milliseconds(0))
-                {
-                    m_timer.expires_from_now(m_time_out);
-                    
-                    m_timer.async_wait([this](boost::system::error_code const& error)
-                    {
-                        this->handle_time_out(error);
-                    });
-                }
-                
-                tcp::resolver resolver(m_io_service);
-                
-                std::string host = (*m_request)[beast::http::field::host].to_string();
-                
-                resolver.async_resolve({host, m_port}, [this](beast::error_code const& error,
-                                                              tcp::resolver::iterator iterator)
-                {
-                    this->handle_resolve(error, iterator);
-                });
-                
-                m_io_service.reset();
-                
-                while(!m_error && m_io_service.run_one())
-                {
-                }
-                
-                error = m_error;
-                
-                return m_response;
-            };
-            
-        private: // methods
-            
-            void handle_time_out(boost::system::error_code const& error)
-            {
-                m_io_service.stop();
-                
-                m_socket.shutdown(tcp::socket::shutdown_both, m_error);
-                m_error = boost::asio::error::basic_errors::timed_out;
-            }
-            
-            void handle_read(beast::error_code const& error)
-            {
-                m_io_service.stop();
-                
-                if (error)
-                {
-                    m_error = error;
-                }
-                else
-                {
-                    m_socket.shutdown(tcp::socket::shutdown_both, m_error);
-                }
-            }
-            
-            void handle_write(beast::error_code const& error)
-            {
-                if (error)
-                {
-                    m_error = error;
-                }
-                else
-                {
-                    beast::http::async_read(m_socket, m_buffer, m_response, [this](beast::error_code const& error)
-                    {
-                        this->handle_read(error);
-                    });
-                }
-            }
-            
-            void handle_connect(beast::error_code const& error)
-            {
-                if (error)
-                {
-                    m_error = error;
-                }
-                else
-                {   
-                    beast::http::async_write(m_socket, *m_request, [this](beast::error_code const& error)
-                    {
-                        this->handle_write(error);
-                    });
-                    
-                }
-            }
-            
-            void handle_resolve(beast::error_code const& error,
-                                tcp::resolver::iterator iterator)
-            {
-                if (error)
-                {
-                    m_error = error;
-                }
-                else
-                {
-                    boost::asio::async_connect(m_socket, iterator, [this](beast::error_code const& error,
-                                                                          tcp::resolver::iterator i)
-                    {
-                        this->handle_connect(error);
-                    });
-                }
-            }
-            
-        private: // members
-            
-            std::unique_ptr<beast::http::request<ReqType>>  m_request;
-            beast::http::response<ResType>                  m_response;
-            std::string                                     m_port;
-            boost::asio::io_service                         m_io_service;
-            tcp::socket                                     m_socket;
-            beast::error_code                               m_error;
-            std::chrono::milliseconds                       m_time_out;
-            boost::asio::steady_timer                       m_timer;
-            beast::flat_buffer                              m_buffer;
-            
-            
-        private: // deleted methods
-            
-            HttpQuery() = delete;
-            HttpQuery(HttpQuery const& other) = delete;
-            HttpQuery(HttpQuery && other) = delete;
-            HttpQuery& operator=(HttpQuery const& other) = delete;
-            HttpQuery& operator=(HttpQuery && other) = delete;
-        };
+    public: // methods
         
         //! @brief Sends an http request.
         //! @details Returns the response generated by the server. The function blocks until response
         //! is received or error occurs.
         template<class ReqType, class ResType>
-        beast::http::response<ResType> httpWrite(std::unique_ptr<beast::http::request<ReqType>> request,
-                                                 std::string port,
-                                                 beast::error_code& error,
-                                                 std::chrono::milliseconds time_out = std::chrono::milliseconds(0))
-        {
-            return HttpQuery<ReqType, ResType>(std::move(request), port).writeRequest(error);
-        }
+        static beast::http::response<ResType>
+        write(std::unique_ptr<beast::http::request<ReqType>> request,
+              std::string port,
+              beast::error_code& error,
+              std::chrono::milliseconds timeout = std::chrono::milliseconds(0));
         
         //! @brief Sends an http request asynchronously.
         //! @details Returns the response generated by the server. The function is non blocking. callback
         //! is called on another thread once response is received or error occurs.
         template <class ReqType, class ResType>
-        std::future<void> httpWriteAsync(std::unique_ptr<beast::http::request<ReqType>> request,
-                                         std::string port,
-                                         std::function<void(beast::http::response<ResType> const& response,
-                                                            beast::error_code const& error)> callback,
-                                          std::chrono::milliseconds time_out = std::chrono::milliseconds(0))
-        {
-            std::unique_ptr<HttpQuery<ReqType, ResType>> query(new HttpQuery<ReqType, ResType>(std::move(request), port, time_out));
-            
-            return std::async(std::launch::async, [query = std::move(query), callback]()
-            {
-                beast::error_code error;
-                
-                beast::http::response<ResType> response = query->writeRequest(error);
-                
-                return callback(response, error);
-            });
-        }
-    }
-}
+        static std::future<void>
+        writeAsync(std::unique_ptr<beast::http::request<ReqType>> request,
+                   std::string port,
+                   std::function<void(beast::http::response<ResType> const& response,
+                                      beast::error_code const& error)> callback,
+                   std::chrono::milliseconds timeout = std::chrono::milliseconds(0));
+        
+    private: // nested classes
+        
+        template<class ReqType, class ResType> class Query;
+        
+    private: // deleted methods
+        
+        Http() = delete;
+        Http(Http const& other) = delete;
+        Http(Http && other) = delete;
+        Http& operator=(Http const& other) = delete;
+        Http& operator=(Http && other) = delete;
+    };
+    
+    // ================================================================================ //
+    //                                     HTTP QUERY                                   //
+    // ================================================================================ //
+    
+    template<class ReqType, class ResType>
+    class Http::Query
+    {
+    public: // methods
+        
+        //! @brief Constructor.
+        Query(std::unique_ptr<beast::http::request<ReqType>> request,
+              std::string port,
+              std::chrono::milliseconds timeout = std::chrono::milliseconds(0));
+        
+        //! @brief Destructor.
+        ~Query();
+        
+        //! @brief Write the request and get the response.
+        beast::http::response<ResType> writeRequest(beast::error_code& error);
+        
+    private: // methods
+        
+        //! @internal
+        void handleTimeout(boost::system::error_code const& error);
+        
+        //! @internal
+        void handleRead(beast::error_code const& error);
+        
+        //! @internal
+        void handleWrite(beast::error_code const& error);
+        
+        //! @internal
+        void handleConnect(beast::error_code const& error);
+        
+        //! @internal
+        void handleResolve(beast::error_code const& error,
+                           tcp::resolver::iterator iterator);
+        
+    private: // members
+        
+        std::unique_ptr<beast::http::request<ReqType>>  m_request;
+        beast::http::response<ResType>                  m_response;
+        std::string                                     m_port;
+        boost::asio::io_service                         m_io_service;
+        tcp::socket                                     m_socket;
+        beast::error_code                               m_error;
+        std::chrono::milliseconds                       m_timeout;
+        boost::asio::steady_timer                       m_timer;
+        beast::flat_buffer                              m_buffer;
+        
+    private: // deleted methods
+        
+        Query() = delete;
+        Query(Query const& other) = delete;
+        Query(Query && other) = delete;
+        Query& operator=(Query const& other) = delete;
+        Query& operator=(Query && other) = delete;
+    };
+    
+}} // namespace kiwi::network
+
+#include "KiwiNetwork_Http.hpp"
---
+++ Modules/KiwiNetwork/KiwiNetwork_Http.hpp
@@ -0,0 +1,198 @@
+/*
+ ==============================================================================
+ 
+ This file is part of the KIWI library.
+ - Copyright (c) 2014-2016, Pierre Guillot & Eliott Paris.
+ - Copyright (c) 2016-2017, CICM, ANR MUSICOLL, Eliott Paris, Pierre Guillot, Jean Millot.
+ 
+ Permission is granted to use this software under the terms of the GPL v3
+ (or any later version). Details can be found at: www.gnu.org/licenses
+ 
+ KIWI is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+ ------------------------------------------------------------------------------
+ 
+ Contact : cicm.mshparisnord@gmail.com
+ 
+ ==============================================================================
+ */
+
+#pragma once
+
+namespace kiwi { namespace network {
+    
+    // ================================================================================ //
+    //                                        HTTP                                      //
+    // ================================================================================ //
+    
+    template<class ReqType, class ResType>
+    beast::http::response<ResType>
+    Http::write(std::unique_ptr<beast::http::request<ReqType>> request,
+                std::string port,
+                beast::error_code& error,
+                std::chrono::milliseconds timeout)
+    {
+        return Query<ReqType, ResType>(std::move(request), port, timeout).writeRequest(error);
+    }
+    
+    template <class ReqType, class ResType>
+    std::future<void>
+    Http::writeAsync(std::unique_ptr<beast::http::request<ReqType>> request,
+                     std::string port,
+                     std::function<void(beast::http::response<ResType> const& response,
+                                        beast::error_code const& error)> callback,
+                     std::chrono::milliseconds timeout)
+    {
+        auto query = std::make_unique<Query<ReqType, ResType>>(std::move(request), port, timeout);
+        
+        return std::async(std::launch::async, [query = std::move(query), cb = std::move(callback)]() {
+            
+            beast::error_code error;
+            beast::http::response<ResType> response = query->writeRequest(error);
+            return cb(response, error);
+            
+        });
+    }
+    
+    // ================================================================================ //
+    //                                     HTTP QUERY                                   //
+    // ================================================================================ //
+    
+    template<class ReqType, class ResType>
+    Http::Query<ReqType, ResType>::Query(std::unique_ptr<beast::http::request<ReqType>> request,
+                                         std::string port,
+                                         std::chrono::milliseconds timeout)
+    : m_request(std::move(request))
+    , m_response()
+    , m_port(port)
+    , m_io_service()
+    , m_socket(m_io_service)
+    , m_error()
+    , m_timeout(timeout)
+    , m_timer(m_io_service)
+    , m_buffer()
+    {
+        ;
+    }
+    
+    template<class ReqType, class ResType>
+    Http::Query<ReqType, ResType>::~Query()
+    {
+        ;
+    }
+    
+    template<class ReqType, class ResType>
+    beast::http::response<ResType>
+    Http::Query<ReqType, ResType>::writeRequest(beast::error_code& error)
+    {
+        if (m_timeout > std::chrono::milliseconds(0))
+        {
+            m_timer.expires_from_now(m_timeout);
+            
+            m_timer.async_wait([this](boost::system::error_code const& error) {
+                this->handleTimeout(error);
+            });
+        }
+        
+        tcp::resolver resolver(m_io_service);
+        
+        const std::string host = m_request->at(beast::http::field::host).to_string();
+        
+        resolver.async_resolve({host, m_port}, [this](beast::error_code const& error,
+                                                      tcp::resolver::iterator iterator) {
+            handleResolve(error, iterator);
+        });
+        
+        m_io_service.reset();
+        
+        // block here until error or success
+        while(!m_error && m_io_service.run_one())
+        {
+            ;
+        }
+        
+        error = m_error;
+        
+        return m_response;
+    };
+    
+    template<class ReqType, class ResType>
+    void
+    Http::Query<ReqType, ResType>::handleTimeout(boost::system::error_code const& error)
+    {
+        m_io_service.stop();
+        
+        m_socket.shutdown(tcp::socket::shutdown_both, m_error);
+        m_error = boost::asio::error::basic_errors::timed_out;
+    }
+    
+    template<class ReqType, class ResType>
+    void
+    Http::Query<ReqType, ResType>::handleRead(beast::error_code const& error)
+    {
+        m_io_service.stop();
+        
+        if (error)
+        {
+            m_error = error;
+        }
+        else
+        {
+            m_socket.shutdown(tcp::socket::shutdown_both, m_error);
+        }
+    }
+    
+    template<class ReqType, class ResType>
+    void
+    Http::Query<ReqType, ResType>::handleWrite(beast::error_code const& error)
+    {
+        if (error)
+        {
+            m_error = error;
+        }
+        else
+        {
+            beast::http::async_read(m_socket, m_buffer, m_response, [this](beast::error_code const& error) {
+                handleRead(error);
+            });
+        }
+    }
+    
+    template<class ReqType, class ResType>
+    void
+    Http::Query<ReqType, ResType>::handleConnect(beast::error_code const& error)
+    {
+        if (error)
+        {
+            m_error = error;
+        }
+        else
+        {
+            beast::http::async_write(m_socket, *m_request, [this](beast::error_code const& error) {
+                handleWrite(error);
+            });
+            
+        }
+    }
+    
+    template<class ReqType, class ResType>
+    void
+    Http::Query<ReqType, ResType>::handleResolve(beast::error_code const& error,
+                                                 tcp::resolver::iterator iterator)
+    {
+        if (error)
+        {
+            m_error = error;
+        }
+        else
+        {
+            boost::asio::async_connect(m_socket, iterator, [this](beast::error_code const& error,
+                                                                  tcp::resolver::iterator i) {
+                this->handleConnect(error);
+            });
+        }
+    }
+    
+}} // namespace kiwi::network
