0a5acdeb9bdd4137830a897fd191db4edab7fe07
eliott PARIS <eliottparis@gmail.com>
2017-08-24 15:49:53 +0200

    temp test 5

--- Client/Source/KiwiApp_Network/KiwiApp_ApiController.cpp
+++ Client/Source/KiwiApp_Network/KiwiApp_ApiController.cpp
@@ -70,12 +70,11 @@ namespace kiwi
                               std::function<void()> success_callback,
                               Api::ErrorCallback error_callback)
     {
-        auto success_cb = [this, success_callback = std::move(success_callback)](Api::AuthUser user)
-        {
+        auto success_cb = [this, success_callback = std::move(success_callback)](Api::AuthUser user){
+            
             auto& scheduler = KiwiApp::useInstance().useScheduler();
             scheduler.schedule([this, success_callback = std::move(success_callback), user = std::move(user)](){
                 
-                /*
                 std::cout << "User " << user._id << " Authenticated !\n";
                 
                 m_auth_user = std::move(user);
@@ -84,7 +83,6 @@ namespace kiwi
                 KiwiApp::commandStatusChanged();
                 
                 success_callback();
-                */
                 
             });
         };
