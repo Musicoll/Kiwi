58961c45ae420ec1dcab71d90bd010d94183cdbb
Pierre <guillotpierre6@gmail.com>
2018-04-17 11:56:53 +0200

    update cmake for linux

--- Scripts/juce_module.cmake
+++ Scripts/juce_module.cmake
@@ -6,11 +6,20 @@ set(JUCE_MODULES_PATH ${PROJECT_SOURCE_DIR}/ThirdParty/Juce/modules)
 # The function adds the sources for a juce module.
 # MODULE_NAME is the name of your module (for example: juce_core)
 function(add_juce_module SOURCES_FILE MODULE_NAME)
-  file(GLOB MODULE_SOURCES
-    ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.cpp
-    ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.hpp
-    ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.c
-    ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.h
-    ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.mm)
+  if(APPLE)
+    file(GLOB MODULE_SOURCES
+      ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.cpp
+      ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.hpp
+      ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.c
+      ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.h
+      ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.mm)
+  else()
+    file(GLOB MODULE_SOURCES
+      ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.cpp
+      ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.hpp
+      ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.c
+      ${JUCE_MODULES_PATH}/${MODULE_NAME}/*.h)
+  endif()
+
   set(${SOURCES_FILE} ${${SOURCES_FILE}} ${MODULE_SOURCES} PARENT_SCOPE)
 endfunction(add_juce_module)
