cdf14de2a8da78e9b35972e6dd57bf5f1b6aad39
eliott PARIS <eliottparis@gmail.com>
2018-06-16 03:55:11 +0200

    update model::Converter

--- Modules/KiwiModel/KiwiModel_Converters/KiwiModel_Converter.cpp
+++ Modules/KiwiModel/KiwiModel_Converters/KiwiModel_Converter.cpp
@@ -24,7 +24,9 @@
 #include <KiwiModel/KiwiModel_Def.h>
 #include <KiwiModel/KiwiModel_Converters/KiwiModel_Converter.h>
 
-#include <KiwiTool/KiwiTool_Atom.h>
+#include <KiwiModel/KiwiModel_Converters/KiwiModel_Converter_v1_v2.cpp>
+#include <KiwiModel/KiwiModel_Converters/KiwiModel_Converter_v2_v3.cpp>
+#include <KiwiModel/KiwiModel_Converters/KiwiModel_Converter_v3_v4.cpp>
 
 namespace kiwi { namespace model {
 
@@ -32,152 +34,75 @@ namespace kiwi { namespace model {
     //                                   CONVERTER                                      //
     // ================================================================================ //
     
-    void Converter::convert_v1_v2(flip::BackEndIR & backend)
+    Converter::Converter()
     {
-        flip::BackEndIR::Type& patcher = backend.root;
-        
-        // removing patcher name.
-        patcher.members.remove_if([](std::pair<std::string, flip::BackEndIR::Type> const& member)
-        {
-            return member.first == "patcher_name";
-        });
-        
-        backend.complete_conversion("v2");
+        addConverter<Converter_v1_v2>();
+        addConverter<Converter_v2_v3>();
+        addConverter<Converter_v3_v4>();
     }
     
-    void Converter::convert_v3_v4(flip::BackEndIR & backend)
+    Converter::~Converter()
+    {}
+    
+    Converter& Converter::use()
     {
-        flip::BackEndIR::Type& patcher = backend.root;
-        
-        // random object conversion:
-        // - remove seed inlet (was third inlet in < v4)
-        // - remove range inlet (second) if range argument is defined
-        
-        walk(patcher, [](flip::BackEndIR::Type& type) {
-            
-            if (type.get_class () != "cicm.kiwi.object.random")
-                return; // abort
-            
-            auto const& text_value = type.member("text").second.value.blob;
-            const std::string text { text_value.begin(), text_value.end() };
-            const auto parsed_text = tool::AtomHelper::parse(text);
-            
-            auto& inlets = type.member("inlets").second.array;
-            
-            if(inlets.size() == 3)
-            {
-                inlets.erase(inlets.begin());
-                
-                const bool has_range_arg = (parsed_text.size() > 1
-                                            && parsed_text[1].isNumber());
-                if(has_range_arg)
-                {
-                    inlets.erase(inlets.begin());
-                }
-            }
-        });
-        
-        backend.complete_conversion("v4");
+        static Converter instance;
+        return instance;
     }
     
-    void Converter::remove_invalid_links(flip::BackEndIR& backend)
+    std::string const& Converter::getLatestVersion()
     {
-        flip::BackEndIR::Type& patcher  = backend.root;
+        static const std::string latest_version = KIWI_MODEL_VERSION_STRING;
+        return latest_version;
+    }
+    
+    bool Converter::canConvertToLatestFrom(std::string const& version)
+    {
+        auto& converters = use().converters();
         
-        auto& objects = patcher.member("objects").second.array;
-        
-        // A link is considered invalid if sender or receiver object does not exist,
-        // or if it's bound to a pin that does not exist.
-        auto is_invalid_link = [&objects](flip::BackEndIR::Type& type) {
-            
-            auto const& sender_ref = type.member("sender_obj").second.value.ref;
-            auto const& receiver_ref = type.member("receiver_obj").second.value.ref;
-            auto const& sender_outlet = type.member("outlet_index").second.value.int_num;
-            auto const& receiver_inlet = type.member("inlet_index").second.value.int_num;
-            
-            bool sender_found = false;
-            bool receiver_found = false;
-            
-            // check sender validity
-            for(auto& object : objects)
-            {
-                auto& type = object.second;
-                
-                if(!sender_found && type.ref == sender_ref)
-                {
-                    sender_found = true;
-                    
-                    const auto outlet_count = type.member("outlets").second.array.size();
-                    if(sender_outlet >= outlet_count)
-                    {
-                        // bad outlet
-                        return true;
-                    }
-                }
-                
-                if(!receiver_found && type.ref == receiver_ref)
-                {
-                    receiver_found = true;
-                    
-                    const auto inlet_count = type.member("inlets").second.array.size();
-                    if(receiver_inlet >= inlet_count)
-                    {
-                        // bad inlet
-                        return true;
-                    }
-                }
-                
-                if(sender_found && receiver_found)
-                {
-                    break;
-                }
-            }
-            
-            return (!sender_found || !receiver_found);
-        };
-        
-        auto& links = patcher.member("links").second.array;
-        
-        auto iter = links.begin();
-        const auto end_iter = links.cend();
-        for(; iter != end_iter;)
+        if(version == getLatestVersion())
         {
-            if (is_invalid_link(iter->second))
+            return true;
+        }
+        
+        for(auto& conversion : converters)
+        {
+            if(conversion->v_from == version)
             {
-                links.erase(iter++);
-            }
-            else
-            {
-                ++iter;
+                if(canConvertToLatestFrom(conversion->v_to))
+                {
+                    return true;
+                }
             }
         }
+        
+        return false;
+    }
+    
+    template<class T>
+    bool Converter::addConverter()
+    {
+        m_converters.emplace_back(std::make_unique<T>());
+        return true;
+    }
+    
+    auto Converter::converters() -> converters_t&
+    {
+        return m_converters;
     }
     
     bool Converter::process(flip::BackEndIR & backend)
     {
-        std::string current_version(KIWI_MODEL_VERSION_STRING);
-        
-        if (current_version.compare(backend.version) >= 0)
+        auto& conversions = use().converters();
+        for(auto& conversion : conversions)
         {
-            if (backend.version == "v1")
+            conversion->process(backend);
+            if(backend.version == getLatestVersion())
             {
-                convert_v1_v2(backend);
-            }
-            
-            // no change from v2 to v3 !
-            
-            if (backend.version == "v2")
-            {
-                backend.complete_conversion("v3");
-            }
-            
-            if (backend.version == "v3")
-            {
-                convert_v3_v4(backend);
-                remove_invalid_links(backend);
+                return true;
             }
         }
         
-        return (backend.version == current_version);
+        return false;
     }
 }}
---
+++ Modules/KiwiModel/KiwiModel_Converters/KiwiModel_Converter_v3_v4.cpp
@@ -0,0 +1,147 @@
+/*
+ ==============================================================================
+ 
+ This file is part of the KIWI library.
+ - Copyright (c) 2014-2016, Pierre Guillot & Eliott Paris.
+ - Copyright (c) 2016-2017, CICM, ANR MUSICOLL, Eliott Paris, Pierre Guillot, Jean Millot.
+ 
+ Permission is granted to use this software under the terms of the GPL v3
+ (or any later version). Details can be found at: www.gnu.org/licenses
+ 
+ KIWI is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+ ------------------------------------------------------------------------------
+ 
+ Contact : cicm.mshparisnord@gmail.com
+ 
+ ==============================================================================
+ */
+
+#include <flip/BackEndIR.h>
+#include <KiwiModel/KiwiModel_Converters/KiwiModel_ConverterBase.h>
+
+#include <KiwiTool/KiwiTool_Atom.h>
+
+namespace kiwi { namespace model {
+    
+    //! @brief v3 to v4 converter
+    //! @details random object conversion:
+    // - remove seed inlet (was third inlet in < v4)
+    // - remove range inlet (second) if range argument is defined
+    // - diconnect invalids links
+    
+    struct Converter_v3_v4 : public ConverterBase
+    {
+        Converter_v3_v4() : ConverterBase("v3", "v4") {}
+
+        bool operator () (flip::BackEndIR& backend) const override
+        {
+            flip::BackEndIR::Type& patcher = backend.root;
+            
+            walk(patcher, [](flip::BackEndIR::Type& type) {
+                
+                if (type.get_class () != "cicm.kiwi.object.random")
+                    return; // abort
+                
+                auto const& text_value = type.member("text").second.value.blob;
+                const std::string text { text_value.begin(), text_value.end() };
+                const auto parsed_text = tool::AtomHelper::parse(text);
+                
+                auto& inlets = type.member("inlets").second.array;
+                
+                if(inlets.size() == 3)
+                {
+                    inlets.erase(inlets.begin());
+                    
+                    const bool has_range_arg = (parsed_text.size() > 1
+                                                && parsed_text[1].isNumber());
+                    if(has_range_arg)
+                    {
+                        inlets.erase(inlets.begin());
+                    }
+                }
+            });
+            
+            remove_invalid_links(backend);
+            return true;
+        }
+        
+        bool remove_invalid_links(flip::BackEndIR& backend) const
+        {
+            flip::BackEndIR::Type& patcher  = backend.root;
+            
+            auto& objects = patcher.member("objects").second.array;
+            
+            // A link is considered invalid if sender or receiver object does not exist,
+            // or if it's bound to a pin that does not exist.
+            auto is_invalid_link = [&objects](flip::BackEndIR::Type& type) {
+                
+                auto const& sender_ref = type.member("sender_obj").second.value.ref;
+                auto const& receiver_ref = type.member("receiver_obj").second.value.ref;
+                auto const& sender_outlet = type.member("outlet_index").second.value.int_num;
+                auto const& receiver_inlet = type.member("inlet_index").second.value.int_num;
+                
+                bool sender_found = false;
+                bool receiver_found = false;
+                
+                // check sender validity
+                for(auto& object : objects)
+                {
+                    auto& type = object.second;
+                    
+                    if(!sender_found && type.ref == sender_ref)
+                    {
+                        sender_found = true;
+                        
+                        const auto outlet_count = type.member("outlets").second.array.size();
+                        if(sender_outlet >= outlet_count)
+                        {
+                            // bad outlet
+                            return false;
+                        }
+                    }
+                    
+                    if(!receiver_found && type.ref == receiver_ref)
+                    {
+                        receiver_found = true;
+                        
+                        const auto inlet_count = type.member("inlets").second.array.size();
+                        if(receiver_inlet >= inlet_count)
+                        {
+                            // bad inlet
+                            return false;
+                        }
+                    }
+                    
+                    if(sender_found && receiver_found)
+                    {
+                        break;
+                    }
+                }
+                
+                return (!sender_found || !receiver_found);
+            };
+            
+            auto& links = patcher.member("links").second.array;
+            
+            auto iter = links.begin();
+            const auto end_iter = links.cend();
+            for(; iter != end_iter;)
+            {
+                if (is_invalid_link(iter->second))
+                {
+                    links.erase(iter++);
+                }
+                else
+                {
+                    ++iter;
+                }
+            }
+            
+            return true;
+        }
+    };
+    
+}}
