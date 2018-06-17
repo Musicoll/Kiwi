92461bbe2d7bade1f277d497b3e70b5037ebea21
eliott PARIS <eliottparis@gmail.com>
2018-06-17 21:22:21 +0200

    bump Kiwi version to v1.0.2

--- Client/Source/KiwiApp.h
+++ Client/Source/KiwiApp.h
@@ -34,8 +34,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Kiwi";
-    const char* const  versionString  = "v1.0.1";
-    const int          versionNumber  = 0x101;
+    const char* const  versionString  = "v1.0.2";
+    const int          versionNumber  = 0x102;
 }
 
 namespace kiwi
