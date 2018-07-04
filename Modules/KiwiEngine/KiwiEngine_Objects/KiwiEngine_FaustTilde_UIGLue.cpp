6140b9d121a5ab03c2d2db8f088d07b0cc457489
Pierre <guillotpierre6@gmail.com>
2018-07-04 12:20:25 +0200

    add support for long parameter name

--- Modules/KiwiEngine/KiwiEngine_Objects/KiwiEngine_FaustTilde_UIGLue.cpp
+++ Modules/KiwiEngine/KiwiEngine_Objects/KiwiEngine_FaustTilde_UIGLue.cpp
@@ -31,57 +31,86 @@ namespace kiwi { namespace engine {
     class FaustTilde::UIGlue : public UI
     {
     public:
-        
+        typedef FAUSTFLOAT param_type;
         
         UIGlue(FaustTilde& owner) : m_owner(owner)
         {
             
         }
         
-        void addButton(const char* label, FAUSTFLOAT* zone) override
+        // The FAUST interface
+        // ================================================================================ //
+        
+        // Active widgets
+        // ================================================================================ //
+        void addButton(const char* label, param_type* zone) override
         {
             addParameter(label, Parameter::Type::Button, zone, 0, 0, 0, 0);
         }
         
-        void addCheckButton(const char* label, FAUSTFLOAT* zone) override
+        void addCheckButton(const char* label, param_type* zone) override
         {
             addParameter(label, Parameter::Type::CheckButton, zone, 0.f, 1.f, 1.f, 0.f);
         }
         
-        void addVerticalSlider(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step) override
+        void addVerticalSlider(const char* label, param_type* zone, param_type init, param_type min, param_type max, param_type step) override
         {
             addParameter(label, Parameter::Type::Float, zone, min, max, step, init);
         }
         
-        virtual void addHorizontalSlider(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step) override
+        virtual void addHorizontalSlider(const char* label, param_type* zone, param_type init, param_type min, param_type max, param_type step) override
         {
             addParameter(label, Parameter::Type::Float, zone, min, max, step, init);
         }
         
-        virtual void addNumEntry(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step) override
+        virtual void addNumEntry(const char* label, param_type* zone, param_type init, param_type min, param_type max, param_type step) override
         {
             addParameter(label, Parameter::Type::Float, zone, min, max, step, init);
         }
         
-        void addHorizontalBargraph(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max) override {};
-        void addVerticalBargraph(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max) override {};
+        // Passive widgets
+        // ================================================================================ //
+        void addHorizontalBargraph(const char* label, param_type* zone, param_type min, param_type max) override {};
+        void addVerticalBargraph(const char* label, param_type* zone, param_type min, param_type max) override {};
         void addSoundfile(const char* label, const char* filename, Soundfile** sf_zone) override {}
         
-        void declare(FAUSTFLOAT*, const char* key, const char* value) override
+        
+        // Widget layout
+        // ================================================================================ //
+        void openTabBox(const char* label) override
+        {
+            m_parameter_level.push_back(label);
+        }
+        
+        void openHorizontalBox(const char* label) override
+        {
+            m_parameter_level.push_back(label);
+        }
+        
+        void openVerticalBox(const char* label) override
+        {
+            m_parameter_level.push_back(label);
+        }
+        
+        void closeBox() override
+        {
+            m_parameter_level.pop_back();
+        }
+        
+        // Meta data
+        // ================================================================================ //
+        void declare(param_type*, const char* key, const char* value) override
         {
             m_owner.log(std::string("faust~: declare ") + std::string(key) + std::string(" - ") + std::string(value));
         }
         
-        void openTabBox(const char* label) override {};
-        void openHorizontalBox(const char* label) override {}
-        void openVerticalBox(const char* label) override {}
-        void closeBox() override {}
+        // ================================================================================ //
         
         void log()
         {
             std::lock_guard<std::mutex> guard(m_mutex_glue);
-            m_owner.log("faust~: number of parameters " + std::to_string(m_parameters.size()));
-            for(auto const& param : m_parameters)
+            m_owner.log("faust~: number of parameters " + std::to_string(m_params_short.size()));
+            for(auto const& param : m_params_short)
             {
                 m_owner.log("faust~: parameter " + param.first + " " + param.second.toString());
             }
@@ -91,81 +120,85 @@ namespace kiwi { namespace engine {
         {
             if(m_mutex_glue.try_lock())
             {
-                auto it = m_parameters.find(name);
-                if(it != m_parameters.end())
+                auto it = m_params_short.find(name);
+                if(it == m_params_short.end())
                 {
-                    if(it->second.dirty)
+                    it = m_params_long.find(name);
+                    if(it == m_params_long.end())
                     {
                         m_mutex_glue.unlock();
-                        m_owner.warning(std::string("FAUST interfaces \"") + name + std::string("\" not valid anymore"));
+                        m_owner.warning(std::string("no such FAUST interface \"") + name + std::string("\""));
                         return;
                     }
-                    if(it->second.type == Parameter::Type::Button)
-                    {
-                        *(it->second.zone) = 0;
-                        *(it->second.zone) = 1;
-                    }
-                    else
-                    {
-                        m_owner.warning(std::string("FAUST interface \"") + name + std::string("\" is a requires a value"));
-                    }
+                }
+                
+                if(it->second.dirty)
+                {
+                    m_mutex_glue.unlock();
+                    m_owner.warning(std::string("FAUST interfaces \"") + name + std::string("\" not valid anymore"));
+                    return;
+                }
+                if(it->second.type == Parameter::Type::Button)
+                {
+                    *(it->second.zone) = 0;
+                    *(it->second.zone) = 1;
                 }
                 else
                 {
-                    m_owner.warning(std::string("no such FAUST interface \"") + name + std::string("\""));
+                    m_owner.warning(std::string("FAUST interface \"") + name + std::string("\" is a requires a value"));
                 }
                 m_mutex_glue.unlock();
+                return;
             }
-            else
-            {
-                m_owner.warning(std::string("FAUST interfaces being processed - please wait"));
-            }
+            m_owner.warning(std::string("FAUST interfaces being processed - please wait"));
         }
         
-        void set(const std::string& name, FAUSTFLOAT value)
+        void set(const std::string& name, param_type value)
         {
             if(m_mutex_glue.try_lock())
             {
-                auto it = m_parameters.find(name);
-                if(it != m_parameters.end())
+                auto it = m_params_short.find(name);
+                if(it == m_params_short.end())
                 {
-                    if(it->second.dirty)
+                    it = m_params_long.find(name);
+                    if(it == m_params_long.end())
                     {
                         m_mutex_glue.unlock();
-                        m_owner.warning(std::string("FAUST interfaces \"") + name + std::string("\" not valid anymore"));
+                        m_owner.warning(std::string("no such FAUST interface \"") + name + std::string("\""));
                         return;
                     }
-                    if(it->second.type == Parameter::Type::CheckButton)
-                    {
-                        *(it->second.zone) = static_cast<FAUSTFLOAT>(value < std::numeric_limits<FAUSTFLOAT>::epsilon());
-                    }
-                    else if(it->second.type == Parameter::Type::Float)
-                    {
-                        const FAUSTFLOAT min = it->second.min;
-                        const FAUSTFLOAT max = it->second.max;
-                        *(it->second.zone) = std::max(std::min(max, value), min);
-                    }
-                    else
-                    {
-                        m_owner.warning(std::string("FAUST interface \"") + name + std::string("\" is doesn't requires a value"));
-                    }
+                }
+                
+                if(it->second.dirty)
+                {
+                    m_mutex_glue.unlock();
+                    m_owner.warning(std::string("FAUST interfaces \"") + name + std::string("\" not valid anymore"));
+                    return;
+                }
+                if(it->second.type == Parameter::Type::CheckButton)
+                {
+                    *(it->second.zone) = static_cast<param_type>(value < std::numeric_limits<param_type>::epsilon());
+                }
+                else if(it->second.type == Parameter::Type::Float)
+                {
+                    const param_type min = it->second.min;
+                    const param_type max = it->second.max;
+                    *(it->second.zone) = std::max(std::min(max, value), min);
                 }
                 else
                 {
-                    m_owner.warning(std::string("no such FAUST interface \"") + name + std::string("\""));
+                    m_owner.warning(std::string("FAUST interface \"") + name + std::string("\" is doesn't requires a value"));
                 }
                 m_mutex_glue.unlock();
+                return;
             }
-            else
-            {
-                m_owner.warning(std::string("FAUST interfaces being processed - please wait"));
-            }
+            m_owner.warning(std::string("FAUST interfaces being processed - please wait"));
         }
         
         void resetToDefault()
         {
             std::lock_guard<std::mutex> guard(m_mutex_glue);
-            for(auto& param : m_parameters)
+            for(auto& param : m_params_short)
             {
                 *param.second.zone = param.second.deft;
             }
@@ -174,7 +207,7 @@ namespace kiwi { namespace engine {
         void saveStates()
         {
             std::lock_guard<std::mutex> guard(m_mutex_glue);
-            for(auto& param : m_parameters)
+            for(auto& param : m_params_short)
             {
                 param.second.saved = *param.second.zone;
             }
@@ -183,7 +216,7 @@ namespace kiwi { namespace engine {
         void recallStates()
         {
             std::lock_guard<std::mutex> guard(m_mutex_glue);
-            for(auto& param : m_parameters)
+            for(auto& param : m_params_short)
             {
                 *param.second.zone = param.second.saved;
             }
@@ -192,26 +225,29 @@ namespace kiwi { namespace engine {
         void prepareChanges()
         {
             std::lock_guard<std::mutex> guard(m_mutex_glue);
-            for(auto& param : m_parameters)
+            for(auto& param : m_params_short)
             {
                 param.second.dirty = true;
             }
+            m_parameter_level.clear();
+            m_params_long.clear();
         }
         
         void finishChanges()
         {
             std::lock_guard<std::mutex> guard(m_mutex_glue);
-            for(auto it = m_parameters.begin(); it != m_parameters.end();)
+            for(auto it = m_params_short.begin(); it != m_params_short.end();)
             {
                 if(it->second.dirty)
                 {
-                    it = m_parameters.erase(it);
+                    it = m_params_short.erase(it);
                 }
                 else
                 {
                     it++;
                 }
             }
+            m_parameter_level.clear();
         }
         
     private:
@@ -228,12 +264,12 @@ namespace kiwi { namespace engine {
             };
             
             Type        type;
-            FAUSTFLOAT* zone;
-            FAUSTFLOAT  min;
-            FAUSTFLOAT  max;
-            FAUSTFLOAT  step;
-            FAUSTFLOAT  deft;
-            FAUSTFLOAT  saved;
+            param_type* zone;
+            param_type  min;
+            param_type  max;
+            param_type  step;
+            param_type  deft;
+            param_type  saved;
             bool        dirty; // If the parameter should be deleted
             
             std::string toString() const
@@ -242,11 +278,19 @@ namespace kiwi { namespace engine {
             }
         };
         
-        void addParameter(const char* name, Parameter::Type type, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step, FAUSTFLOAT init)
+        std::string getLongName(const char* name)
+        {
+            std::string longname;
+            for(auto const& step : m_parameter_level)
+                longname += step + "/";
+            return longname + name;
+        }
+        
+        void addParameter(const char* name, Parameter::Type type, param_type* zone, param_type min, param_type max, param_type step, param_type init)
         {
             std::lock_guard<std::mutex> guard(m_mutex_glue);
-            auto it = m_parameters.find(name);
-            if(it != m_parameters.end())
+            auto it = m_params_short.find(name);
+            if(it != m_params_short.end())
             {
                 if(it->second.type != type ||
                    it->second.min != min ||
@@ -255,6 +299,7 @@ namespace kiwi { namespace engine {
                    it->second.deft != init)
                 {
                     it->second = Parameter({type, zone, min, max, step, init, init, false});
+                    m_params_long[getLongName(name)] = it->second;
                     *zone = init;
                 }
                 else
@@ -262,18 +307,22 @@ namespace kiwi { namespace engine {
                     it->second.saved = *it->second.zone;
                     it->second.zone = zone;
                     it->second.dirty = false;
+                    m_params_long[getLongName(name)] = it->second;
                     *zone = it->second.saved;
                 }
             }
             else
             {
-                m_parameters[name] = Parameter({type, zone, min, max, step, init, init, false});
+                m_params_short[name] = Parameter({type, zone, min, max, step, init, init, false});
+                m_params_long[getLongName(name)] = it->second;
                 *zone = init;
             }
         }
-        
-        std::map<std::string, Parameter> m_parameters;
-        FaustTilde&                      m_owner;
-        std::mutex                       m_mutex_glue;
+
+        std::map<std::string, Parameter>    m_params_short;
+        std::map<std::string, Parameter>    m_params_long;
+        std::vector<std::string>            m_parameter_level;
+        FaustTilde&                         m_owner;
+        std::mutex                          m_mutex_glue;
     };
 }}
