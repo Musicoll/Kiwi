d1270d19307844a3d452dff7dda65a1959f58eda
Pierre <guillotpierre6@gmail.com>
2018-07-10 10:32:58 +0200

    updated version to 1.0.3

--- Client/Source/KiwiApp.h
+++ Client/Source/KiwiApp.h
@@ -34,8 +34,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Kiwi";
-    const char* const  versionString  = "v1.0.2";
-    const int          versionNumber  = 0x102;
+    const char* const  versionString  = "v1.0.3";
+    const int          versionNumber  = 0x103;
 }
 
 namespace kiwi
