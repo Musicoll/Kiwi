e120121f3543b121ce0dec8d490446f09e618e08
Pierre <guillotpierre6@gmail.com>
2018-07-25 12:31:17 +0200

    Idem.

--- Modules/KiwiEngine/KiwiEngine_Objects/KiwiEngine_FaustTilde.cpp
+++ Modules/KiwiEngine/KiwiEngine_Objects/KiwiEngine_FaustTilde.cpp
@@ -63,9 +63,9 @@ namespace kiwi { namespace engine {
     m_user_id(getUserId(model))
     {
         m_compile_options.push_back("-I");
-        auto apppath = juce::File::getSpecialLocation(juce::File::SpecialLocationType::currentApplicationFile).getFullPathName().toStdString();
+        auto apppath = juce::File::getSpecialLocation(juce::File::SpecialLocationType::currentApplicationFile).getParentDirectory().getFullPathName().toStdString();
 #ifdef __APPLE__
-        m_compile_options.push_back(apppath + std::string("/Contents/Resources/Faust/Libs"));
+        m_compile_options.push_back(juce::File::getSpecialLocation(juce::File::SpecialLocationType::currentApplicationFile).getFullPathName().toStdString() + std::string("/Contents/Resources/Faust/Libs"));
 #elif _WIN32
         std::replace(apppath.begin(), apppath.end(), '\\', '/');
         m_compile_options.push_back(apppath + std::string("/Faust/Libs"));
